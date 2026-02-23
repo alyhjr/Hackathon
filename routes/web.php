cd2c842d96e17832e35e72d466ddad8e6ce05c55
aaliyah jahra maharani <jahramaharani15@gmail.com>
2026-02-23 15:16:50 +0700

    save routes update before going home

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// BERANDA
 Route::view('/', 'pages.home')->name('home');
 
-Route::view('/lomba/panduan', 'pages.lomba_panduan')->name('lomba.panduan');
-Route::view('/lomba/tahapan', 'pages.lomba_tahapan')->name('lomba.tahapan');
+// LOMBA
+Route::prefix('lomba')->name('lomba.')->group(function () {
+    Route::view('/panduan', 'pages.lomba.panduan')->name('panduan');
+    Route::view('/tahapan', 'pages.lomba.tahapan')->name('tahapan');
+});
 
-Route::view('/pengumuman/3besar', 'pages.pengumuman_3besar')->name('pengumuman.3besar');
-Route::view('/pengumuman/lolos', 'pages.pengumuman_lolos')->name('pengumuman.lolos');
+// PENGUMUMAN
+Route::prefix('pengumuman')->name('pengumuman.')->group(function () {
+    Route::view('/3besar', 'pages.pengumuman.3besar')->name('3besar');
+    Route::view('/lolos-seleksi-proposal', 'pages.pengumuman.lolos')->name('lolos');
+});
 
+// FAQ
 Route::view('/faq', 'pages.faq')->name('faq');
